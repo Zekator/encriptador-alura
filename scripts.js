0ddef34e84a5a18dcdfa8bd0fe1af36723c50f31
Emanuel <emaliberal@gmail.com>
2023-05-31 00:25:03 -0300

    Emprolijamos un poco el archivo style. Realizamos cambios en HTML-Logramos cambiar el color de la letra del placeholder

---
+++ scripts.js
@@ -0,0 +1,10 @@
+/*Aca va la logica del Encriptador*/
+
+function encrypt(){
+    let text = document.getElementById("text-box")
+}
+
+
+function myFunction() {
+    document.getElementById("myDIV").style.display = "none";
+  }
